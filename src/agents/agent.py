Fixed exploding gradients, added sampling strategy

src/agents/agent.py
@@ -25,6 +25,8 @@ class Agent(ABC):
         dist[action_idx] = 1.0
         return Categorical(dist)
 
+    def sampling_strategy(self, dist: Categorical):
+        return dist.sample()
 
 # ======================================================================
 class RandomAgent(Agent):
@@ -38,9 +40,10 @@ class RandomAgent(Agent):
 # ======================================================================
 class TrainableAgent(Agent, ABC):
     @abstractmethod
-    def __init__(self, act_space_size):
+    def __init__(self, act_space_size, epsilon=0.1):
         super().__init__(act_space_size)
         self.model: torch.nn.Module = None
+        self.epsilon = epsilon
         self.is_training = True
 
     @abstractmethod
@@ -84,3 +87,15 @@ class TrainableAgent(Agent, ABC):
 
     def load(self, path):
         self.model = torch.jit.load(path)
+
+    def sampling_strategy(self, dist: Categorical):
+        if self.is_training:
+            return self._training_sampling(dist)
+        return torch.argmax(dist.probs)
+
+    def _training_sampling(self, dist: Categorical):
+        # Epsilon greedy
+        if self.rng.uniform() < self.epsilon:
+            return torch.randint(low=0, high=self.act_space_size, size=(dist.probs.size(0), ))
+        return dist.sample()
+

src/agents/ppo.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 from torch import autocast, nn
 from torch.distributions import Categorical
+from torch.nn.functional import mse_loss
 from torchinfo import summary
 from tqdm.auto import tqdm
 
@@ -22,22 +23,21 @@ class PPONet(nn.Module):
             nn.Linear(n_features, n_features // 4),
             nn.LeakyReLU(0.01, inplace=True),
             nn.Linear(n_features // 4, 1),
+            nn.Tanh()
         )
 
     def forward(self, x):
         feats = self.feature_extractor(x)
         actions = self.action_net(feats)
-        values = self.value_net(feats).flatten()
+        values = self.value_net(feats).flatten() * 2
         return actions, values
 
 
 class PPOAgent(TrainableAgent):
-    def __init__(self, act_space_size, eps=0.2, clip_eps=0.2):
-        super().__init__(act_space_size)
-        self.is_training = True
-        self.eps = eps
+    def __init__(self, act_space_size, epsilon=0.02, clip_eps=0.2):
+        super().__init__(act_space_size, epsilon)
         self.clip_eps = clip_eps
-        self.model = PPONet(64, 2, act_space_size)
+        self.model = PPONet(64, 1, act_space_size)
 
     def act(self, obs: torch.Tensor, add_rand=True):
         # Forward
@@ -55,17 +55,20 @@ class PPOAgent(TrainableAgent):
     def loss(self, trajectory: "TensorTrajectory"):
         action_dist, state_values = self.act(trajectory.obs)
         ratio = action_dist.probs / trajectory.probs
-        a = action_dist.sample()  # or use argmax?
+        a = trajectory.actions
         advantage = trajectory.rewards - state_values
+        # advantage = trajectory.rewards
 
         e = self.clip_eps
         r = ratio[:, a]
-        l_clip = torch.min(
+        l_clip = torch.minimum(
             r * advantage,
             torch.clip(r, 1 - e, 1 + e) * advantage,
         )
+        l_vf = 0.1 * mse_loss(state_values, trajectory.rewards, reduction='none')
         l_s = 0.1 * action_dist.entropy()
-        l_clip_s = - torch.sum(l_clip + l_s)
+        # l_s = 0.1 * torch.clip(action_dist.entropy(), 0, 1000)
+        l_clip_s = -torch.sum(l_clip - l_vf + l_s)
         return l_clip_s
 
 

src/replay_buffer.py
@@ -140,9 +140,11 @@ class ReplayBuffer:
             self.trajectory = self.trajectory.tensor()
         return self.trajectory
 
-    def generate_vectorized(self, venv: gym3.Env, agent: Agent, device, progress_bar=False, enable_grad=False):
+    def generate_vectorized(self, venv: gym3.Env, agent: Agent, device, progress_bar=False, enable_grad=False, sampling_strategy=None):
         self.reset()
         steps = trange(self.buffer_size) if progress_bar else range(progress_bar)
+        sampling_strategy = sampling_strategy if sampling_strategy is not None else lambda x: x.sample()
+
         with torch.set_grad_enabled(enable_grad):
             for _ in steps:
                 rew, obs, first = venv.observe()
@@ -158,7 +160,7 @@ class ReplayBuffer:
                 else:
                     action_dist = agent_output
                     values = torch.zeros(obs.size(0))
-                chosen_actions = action_dist.sample()
+                chosen_actions = agent.sampling_strategy(action_dist)
                 probs = action_dist.probs
                 venv.act(chosen_actions.cpu().detach().numpy())
 

src/trainer.py
@@ -4,15 +4,15 @@ from tqdm.auto import trange
 
 from agents.agent import TrainableAgent
 from replay_buffer import ReplayBuffer
-from utils import discount, generate_environment, generate_vec_environment, obs_to_tensor
+from utils import discount, generate_environment, generate_vec_environment, obs_to_tensor, standardize
 
 
 def train(
         agent: TrainableAgent,
         env_name,
-        n_episodes=10,
-        n_parallel=128,
-        buffer_size=512,
+        n_episodes=25,
+        n_parallel=32,
+        buffer_size=1024,
         batch_size=128,
 ):
     # Initialize the environment
@@ -21,7 +21,7 @@ def train(
 
     # Initialize torch stuff
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    optimizer = torch.optim.RAdam(agent.parameters, lr=1e-5, weight_decay=0.01)
+    optimizer = torch.optim.AdamW(agent.parameters, lr=3e-4, weight_decay=0.02, amsgrad=True)
     agent.compile(device)
     agent.train()
 
@@ -30,10 +30,16 @@ def train(
         agent.reset()
         episodes = replay_buffer.generate_vectorized(venv, agent, device, progress_bar=True).episodic()
 
-        # Compute discount rewards
+        # (logging)
+        reward_sum = torch.sum(torch.cat(episodes.rewards)).item()
         mean_reward: torch.Tensor = torch.mean(torch.cat(episodes.rewards)).item()
+
+        # Discount the rewards
         episodes.rewards = discount(episodes.rewards, gamma=0.99)
         mean_discounted = torch.mean(torch.cat(episodes.rewards)).item()
+
+        # Standardize the rewards
+        episodes.rewards = standardize(episodes.rewards, eps=0.01)
 
         # Do a training step
         n_losses = 0
@@ -47,10 +53,12 @@ def train(
             losses.append(loss.item())
             optimizer.zero_grad()
             loss.backward()
+            # torch.nn.utils.clip_grad_norm_(agent.parameters, 5)
             optimizer.step()
 
 
         print()
+        print("Reward sum", reward_sum)
         print("Mean reward", mean_reward)
         print("Mean discounted reward", mean_discounted)
         print("Loss", np.mean(losses))
@@ -75,7 +83,7 @@ def evaluate(agent, env_name, n_episodes=10):
                     break
 
 
-def train_eval(agent: TrainableAgent, save_path, env_name="coinrun"):
+def train_eval(agent: TrainableAgent, save_path, env_name="bossfight"):
     agent = train(agent, env_name=env_name)
     agent.save(save_path)
     input("Waiting for input")